e2ea84db184242d8e38b1fea9b450f0089438117
Harry6542 <ozc189@usak.ca>
2023-07-18 17:41:59 -0600

    code and docstring for to_string()

--- a4q3.py
+++ a4q3.py
@@ -7,3 +7,16 @@ import node as n
 
 
 def to_string(node_chain):
+    """
+       Create a string presentation of the node chain.
+       :param node_chain: The node chain
+       :return: A string representation of the nodes
+       """
+    if node_chain is None:
+        return "EMPTY"
+    elif node_chain.get_next() is None:
+        return "[ " + str(node_chain.get_data()) + " | / ]"
+    else:
+        current_chain_data = "[ " + str(node_chain.get_data()) + " | *-]-->"
+        return current_chain_data + to_string(node_chain.get_next())
+
