52a6992bf619fa649ea585c49848802dc7e21d41
Harry6542 <ozc189@usak.ca>
2023-07-18 18:35:38 -0600

    def test_to_string(),def test_copy(),def test_replace()

--- a4q3.py
+++ a4q3.py
@@ -49,3 +49,53 @@ def replace(node_chain, target, replacement):
             node_chain.set_data(replacement)
         replace(node_chain.get_next(), target, replacement)
         return node_chain
+def test_to_string():
+    print("Testing to_string function:")
+
+    # An empty chain
+    chain1 = None
+    print("Chain 1:", to_string(chain1))
+
+    # A chain with one node
+    chain2 = n.Node(5)
+    print("Chain 2:", to_string(chain2))
+
+    # A chain with multiple nodes
+    chain3 = n.Node(1, n.Node(2, n.Node(3)))
+    print("Chain 3:", to_string(chain3))
+
+
+def test_copy():
+    print("Testing copy function:")
+
+    # An empty chain
+    chain1 = None
+    copied_chain1 = copy(chain1)
+    print("Copied Chain 1:", to_string(copied_chain1))
+
+    # A chain with one node
+    chain2 = n.Node(5)
+    copied_chain2 = copy(chain2)
+    print("Copied Chain 2:", to_string(copied_chain2))
+
+    # A chain with multiple nodes
+    chain3 = n.Node(1, n.Node(2, n.Node(3)))
+    copied_chain3 = copy(chain3)
+    print("Copied Chain 3:", to_string(copied_chain3))
+def test_replace():
+    print("Testing replace function:")
+
+    # An empty chain
+    chain1 = None
+    replaced_chain1 = replace(chain1, 0, 9)
+    print("Replaced Chain 1:", to_string(replaced_chain1))
+
+    # A chain with no replacements
+    chain2 = n.Node(1, n.Node(2, n.Node(3)))
+    replaced_chain2 = replace(chain2, 0, 9)
+    print("Replaced Chain 2:", to_string(replaced_chain2))
+
+    # A chain with several replacements
+    chain3 = n.Node(1, n.Node(2, n.Node(3, n.Node(1, n.Node(4)))))
+    replaced_chain3 = replace(chain3, 1, 9)
+    print("Replaced Chain 3:", to_string(replaced_chain3))
