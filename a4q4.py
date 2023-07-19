d815006294fa464b6fb042ff0ef2a3228d8a7e32
Harry6542 <ozc189@usak.ca>
2023-07-18 18:59:27 -0600

    if condition for the function solveMaze()

--- a4q4.py
+++ a4q4.py
@@ -21,3 +21,12 @@ def SolveMaze(m, s, g):
         return False
 
     m[row][col] = 'P'
+    if SolveMaze(m, (row - 1, col), g) or \
+            SolveMaze(m, (row, col - 1), g) or \
+            SolveMaze(m, (row + 1, col), g) or \
+            SolveMaze(m, (row, col + 1), g):
+        return True
+
+        # Backtrack by removing current location from path
+        m[row][col] = '0'
+        return False
