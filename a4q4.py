3710eceff899204e7b331bd0d4e4f202b5689567
Harry6542 <ozc189@usak.ca>
2023-07-18 18:47:03 -0600

    def read_maze()

--- a4q4.py
+++ a4q4.py
@@ -2,3 +2,7 @@
 #NSID-ozc189
 #course-CMPT145
 #Instructor Name-Laurissa Stilling
+def read_maze(file_name):
+    with open(file_name, 'r') as file:
+        maze_data = [list(line.strip().split()) for line in file]
+    return maze_data
