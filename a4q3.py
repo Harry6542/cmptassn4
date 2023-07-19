955fb9fab5d6f406b266d3c3f5b199c589642ec5
Harry6542 <ozc189@usak.ca>
2023-07-18 18:03:54 -0600

    code and docstring for to_string()

--- a4q3.py
+++ a4q3.py
@@ -19,4 +19,18 @@ def to_string(node_chain):
     else:
         current_chain_data = "[ " + str(node_chain.get_data()) + " | *-]-->"
         return current_chain_data + to_string(node_chain.get_next())
-
+def copy(node_chain):
+    """
+    Make a completely new copy of the given node chain.
+    :param node_chain: The node chain
+    :return: A copy of the new node chain
+    """
+    if node_chain is None:
+        return None
+    elif node_chain.get_next() is None:
+        return n.Node(node_chain.get_data())
+    else:
+        new_node_chain = n.Node(node_chain.get_data())
+        new_chain_next = copy(node_chain.get_next())
+        new_node_chain.set_next(new_chain_next)
+        return new_node_chain
