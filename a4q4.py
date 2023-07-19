ecc319cecb58cd9beaecf255fb5dd25eaf50342a
Harry6542 <ozc189@usak.ca>
2023-07-18 18:50:54 -0600

    def print_maze()

--- a4q4.py
+++ a4q4.py
@@ -6,3 +6,6 @@ def read_maze(file_name):
     with open(file_name, 'r') as file:
         maze_data = [list(line.strip().split()) for line in file]
     return maze_data
+def print_maze(maze):
+    for row in maze:
+        print(' '.join(row))
